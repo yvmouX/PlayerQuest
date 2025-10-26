eaa2d0053ed47dc20f8f717346d70daaaed75fd6
yvmou_ <yvmou@outlook.com>
2025-10-26 17:36:22 +0800

    基础存储

---
+++ src/main/java/com/playerPlugin/playerTaskX/storge/Storge.java
@@ -0,0 +1,11 @@
+package com.playerPlugin.playerTaskX.storge;
+
+public interface Storge {
+    void init();
+
+    void sava();
+
+    void load();
+
+    void close();
+}
---
+++ src/main/java/com/playerPlugin/playerTaskX/storge/StorgeType.java
@@ -0,0 +1,17 @@
+package com.playerPlugin.playerTaskX.storge;
+
+import com.playerPlugin.playerTaskX.PlayerTaskX;
+
+public class StorgeType {
+    private final PlayerTaskX plugin;
+
+    public StorgeType(PlayerTaskX plugin) {
+        this.plugin = plugin;
+    }
+
+    public Storge create() {
+        final String pass;
+        //...
+        return null;
+    }
+}
