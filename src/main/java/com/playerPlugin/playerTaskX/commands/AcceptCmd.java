9dcd10c7aae8fd6d999c4eb827a4a1a78164a66c
yvmou_ <yvmou@outlook.com>
2025-10-26 22:18:29 +0800

    add: 新增命令 accept

---
+++ src/main/java/com/playerPlugin/playerTaskX/commands/AcceptCmd.java
@@ -0,0 +1,43 @@
+package com.playerPlugin.playerTaskX.commands;
+
+import cn.yvmou.ylib.api.command.CommandOptions;
+import cn.yvmou.ylib.api.command.SubCommand;
+import com.playerPlugin.playerTaskX.PlayerTask.TaskManager;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/**
+ * 接受 CMD
+ *
+ * <p>
+ *     接受一个任务
+ *     仅玩家可用
+ * </p>
+ *
+ * @author yvmoux
+ * @date 2025/10/26
+ */
+public class AcceptCmd implements SubCommand {
+    @Override
+    @CommandOptions(name = "accept", permission = "playertaskx.command.accept", onlyPlayer = true, alias = {}, register = true, usage = "playertaskx accept <任务ID>")
+    public boolean execute(CommandSender sender, String[] args) {
+        accept(sender, args);
+        return true;
+    }
+
+    private boolean accept(CommandSender sender, String[] args) {
+        Player player = (Player) sender;
+
+        TaskManager taskManager = TaskManager.getInstance();
+        if (taskManager == null) {
+            player.sendMessage("§c任务管理器未初始化！\n" +
+                    "这是个不应该发生的错误，如果您看到这个消息，请及时服务器报告管理员！");
+            return true;
+        }
+
+        String taskId = args[1];
+        taskManager.startTask(player, taskId);
+
+        return true;
+    }
+}
