b4bbf90c3823929b2de7540597c0784a70d9c6ee
yvmou_ <yvmou@outlook.com>
2025-10-28 21:24:54 +0800

    fea & test: 网页编辑器

---
+++ playertaskx-editor/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+import Nav from "@/components/Nav.vue";
+</script>
+
+<template>
+  <Nav logo="logo" item1="首页" item2="关于" item3="联系"/>
+  <body>
+  <p class="text-2xl">什么也没有！！！！！</p>
+  </body>
+</template>
---
+++ playertaskx-editor/src/main.ts
@@ -0,0 +1,7 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import './base/tailwind.css'
+
+createApp(App).mount('#app')
