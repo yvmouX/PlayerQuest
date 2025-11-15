de02c8b9586c7604b7b7f95ca5ee95a13ea2322a
yvmou_ <yvmou@outlook.com>
2025-11-16 00:17:28 +0800

    init

--- src/main/java/com/playerPlugin/playerTaskX/consts/TaskTriggerSpecialProperties.java
+++
@@ -1,7 +0,0 @@
-package com.playerPlugin.playerTaskX.consts;
-
-public class TaskTriggerSpecialProperties {
-    public static final String SP_DAMAGE = "D";
-    public static final String SP_HEALTH = "H";
-    public static final String SP_ARMOR = "A";
-}
--- src/main/java/com/playerPlugin/playerTaskX/dataManager/StorgeTypes.java
+++
@@ -1,6 +0,0 @@
-package com.playerPlugin.playerTaskX.dataManager;
-
-public enum StorgeTypes {
-    SQLITE,
-    MYSQL
-}
