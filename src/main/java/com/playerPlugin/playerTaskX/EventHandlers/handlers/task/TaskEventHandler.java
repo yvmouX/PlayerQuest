9c1b4e492ce5ed38d1cec30464a9b469f403e6ad
Findoutsider <2275016544@qq.com>
2025-11-15 13:05:01 +0800

    fea: 完成大部分事件的监听

--- src/main/java/com/playerPlugin/playerTaskX/EventHandlers/handlers/task/TaskEventHandler.java
+++ src/main/java/com/playerPlugin/playerTaskX/EventHandlers/handlers/task/TaskEventHandler.java
@@ -2,48 +2,128 @@ package com.playerPlugin.playerTaskX.EventHandlers.handlers.task;
 
 import com.playerPlugin.playerTaskX.EventHandlers.EventCallbackManager;
 import com.playerPlugin.playerTaskX.structs.eventStructs.BreakEvent;
+import com.playerPlugin.playerTaskX.structs.eventStructs.EnchantEvent;
+import com.playerPlugin.playerTaskX.structs.eventStructs.FishingEvent;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.enchantment.EnchantItemEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.EntityTameEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.player.PlayerFishEvent;
+import org.bukkit.event.player.PlayerShearEntityEvent;
 
 public class TaskEventHandler implements Listener {
     EventCallbackManager cbm = EventCallbackManager.getInstance();
 
-    @EventHandler
-    public void onPlayerBreak(BlockBreakEvent event) {
-        cbm.triggerCallbacks(new BreakEvent(
-                event.getPlayer(), event.getBlock(),
-                event.getBlock().getType(),
-                event.getBlock().getLocation()
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerBreak(BlockBreakEvent e) {
+        if (e.isCancelled()) return;
+        cbm.triggerCallbacks(new BlockBreakEvent(
+                e.getBlock(),
+                e.getPlayer()
         ));
     }
-    
-    @EventHandler
-    @SuppressWarnings("deprecation")
+
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onPlayerPlace(BlockPlaceEvent e) {
+        if (e.isCancelled()) return;
         cbm.triggerCallbacks(new BlockPlaceEvent(
                 e.getBlockPlaced(),
                 e.getBlockReplacedState(),
                 e.getBlockAgainst(),
                 e.getItemInHand(),
                 e.getPlayer(),
-                e.canBuild()
+                e.canBuild(),
+                e.getHand()
         ));
     }
 
-    @EventHandler
-    public void onPlayerDeath(PlayerDeathEvent e) {
-        cbm.triggerCallbacks(new PlayerDeathEvent(
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerDeath(EntityDeathEvent e) {
+        cbm.triggerCallbacks(new EntityDeathEvent(
                 e.getEntity(),
                 e.getDamageSource(),
                 e.getDrops(),
-                e.getDroppedExp(),
-                e.getDeathMessage()
+                e.getDroppedExp()
         ));
     }
-    // TODO
 
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerEnchant(EnchantItemEvent e) {
+        if (e.isCancelled()) return;
+        cbm.triggerCallbacks(new EnchantItemEvent(
+                e.getEnchanter(),
+                e.getView(),
+                e.getEnchantBlock(),
+                e.getItem(),
+                e.getExpLevelCost(),
+                e.getEnchantsToAdd(),
+                e.getEnchantmentHint(),
+                e.getLevelHint(),
+                e.whichButton()
+        ));
+    }
 
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerFishing(PlayerFishEvent e) {
+        if (e.isCancelled()) return;
+        cbm.triggerCallbacks(new PlayerFishEvent(
+                e.getPlayer(),
+                e.getCaught(),
+                e.getHook(),
+                e.getHand(),
+                e.getState()
+        ));
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerCraft(CraftItemEvent e) {
+        if (e.isCancelled()) return;
+        cbm.triggerCallbacks(new CraftItemEvent(
+                e.getRecipe(),
+                e.getView(),
+                e.getSlotType(),
+                e.getSlot(),
+                e.getClick(),
+                e.getAction(),
+                e.getHotbarButton()
+        ));
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerFoolLevelChange(FoodLevelChangeEvent e) {
+        if (e.isCancelled()) return;
+        cbm.triggerCallbacks(new FoodLevelChangeEvent(
+                e.getEntity(),
+                e.getFoodLevel(),
+                e.getItem()
+        ));
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerShear(PlayerShearEntityEvent e) {
+        if (e.isCancelled()) return;
+        cbm.triggerCallbacks(new PlayerShearEntityEvent(
+                e.getPlayer(),
+                e.getEntity(),
+                e.getItem(),
+                e.getHand()
+        ));
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerTame(EntityTameEvent e) {
+        if (e.isCancelled() || !(e.getOwner() instanceof Player)) return;
+        cbm.triggerCallbacks(new EntityTameEvent(
+                e.getEntity(),
+                e.getOwner()
+        ));
+    }
 }
