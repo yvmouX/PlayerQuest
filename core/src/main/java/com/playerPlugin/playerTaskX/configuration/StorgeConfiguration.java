14ebc702fb689d625985fb761880ee7a6f3d9d29
yvmou_ <yvmou@outlook.com>
2025-12-21 01:30:25 +0800

    忘记生成javaBean了

--- core/src/main/java/com/playerPlugin/playerTaskX/configuration/StorgeConfiguration.java
+++ core/src/main/java/com/playerPlugin/playerTaskX/configuration/StorgeConfiguration.java
@@ -131,4 +131,303 @@ cache:
         description = "缓存过期时间（秒）。"
     )
     private int cache_ttl = 300;
+
+    public StorgeConfiguration() {
+    }
+
+    public StorgeConfiguration(String storageMethod_playerData, String storageMethod_questData, String sqlite_file, String mysql_host, int mysql_port, String mysql_database, String mysql_username, String mysql_password, boolean mysql_useSsl, int mysql_poolSize, int connectionPool_maxPoolSize, int connectionPool_minIdle, int connectionPool_connectionTimeout, int connectionPool_idleTimeout, int connectionPool_maxLifetime, boolean cache_enabled, int cache_ttl) {
+        this.storageMethod_playerData = storageMethod_playerData;
+        this.storageMethod_questData = storageMethod_questData;
+        this.sqlite_file = sqlite_file;
+        this.mysql_host = mysql_host;
+        this.mysql_port = mysql_port;
+        this.mysql_database = mysql_database;
+        this.mysql_username = mysql_username;
+        this.mysql_password = mysql_password;
+        this.mysql_useSsl = mysql_useSsl;
+        this.mysql_poolSize = mysql_poolSize;
+        this.connectionPool_maxPoolSize = connectionPool_maxPoolSize;
+        this.connectionPool_minIdle = connectionPool_minIdle;
+        this.connectionPool_connectionTimeout = connectionPool_connectionTimeout;
+        this.connectionPool_idleTimeout = connectionPool_idleTimeout;
+        this.connectionPool_maxLifetime = connectionPool_maxLifetime;
+        this.cache_enabled = cache_enabled;
+        this.cache_ttl = cache_ttl;
+    }
+
+    /**
+     * 获取
+     * @return storageMethod_playerData
+     */
+    public String getStorageMethod_playerData() {
+        return storageMethod_playerData;
+    }
+
+    /**
+     * 设置
+     * @param storageMethod_playerData
+     */
+    public void setStorageMethod_playerData(String storageMethod_playerData) {
+        this.storageMethod_playerData = storageMethod_playerData;
+    }
+
+    /**
+     * 获取
+     * @return storageMethod_questData
+     */
+    public String getStorageMethod_questData() {
+        return storageMethod_questData;
+    }
+
+    /**
+     * 设置
+     * @param storageMethod_questData
+     */
+    public void setStorageMethod_questData(String storageMethod_questData) {
+        this.storageMethod_questData = storageMethod_questData;
+    }
+
+    /**
+     * 获取
+     * @return sqlite_file
+     */
+    public String getSqlite_file() {
+        return sqlite_file;
+    }
+
+    /**
+     * 设置
+     * @param sqlite_file
+     */
+    public void setSqlite_file(String sqlite_file) {
+        this.sqlite_file = sqlite_file;
+    }
+
+    /**
+     * 获取
+     * @return mysql_host
+     */
+    public String getMysql_host() {
+        return mysql_host;
+    }
+
+    /**
+     * 设置
+     * @param mysql_host
+     */
+    public void setMysql_host(String mysql_host) {
+        this.mysql_host = mysql_host;
+    }
+
+    /**
+     * 获取
+     * @return mysql_port
+     */
+    public int getMysql_port() {
+        return mysql_port;
+    }
+
+    /**
+     * 设置
+     * @param mysql_port
+     */
+    public void setMysql_port(int mysql_port) {
+        this.mysql_port = mysql_port;
+    }
+
+    /**
+     * 获取
+     * @return mysql_database
+     */
+    public String getMysql_database() {
+        return mysql_database;
+    }
+
+    /**
+     * 设置
+     * @param mysql_database
+     */
+    public void setMysql_database(String mysql_database) {
+        this.mysql_database = mysql_database;
+    }
+
+    /**
+     * 获取
+     * @return mysql_username
+     */
+    public String getMysql_username() {
+        return mysql_username;
+    }
+
+    /**
+     * 设置
+     * @param mysql_username
+     */
+    public void setMysql_username(String mysql_username) {
+        this.mysql_username = mysql_username;
+    }
+
+    /**
+     * 获取
+     * @return mysql_password
+     */
+    public String getMysql_password() {
+        return mysql_password;
+    }
+
+    /**
+     * 设置
+     * @param mysql_password
+     */
+    public void setMysql_password(String mysql_password) {
+        this.mysql_password = mysql_password;
+    }
+
+    /**
+     * 获取
+     * @return mysql_useSsl
+     */
+    public boolean isMysql_useSsl() {
+        return mysql_useSsl;
+    }
+
+    /**
+     * 设置
+     * @param mysql_useSsl
+     */
+    public void setMysql_useSsl(boolean mysql_useSsl) {
+        this.mysql_useSsl = mysql_useSsl;
+    }
+
+    /**
+     * 获取
+     * @return mysql_poolSize
+     */
+    public int getMysql_poolSize() {
+        return mysql_poolSize;
+    }
+
+    /**
+     * 设置
+     * @param mysql_poolSize
+     */
+    public void setMysql_poolSize(int mysql_poolSize) {
+        this.mysql_poolSize = mysql_poolSize;
+    }
+
+    /**
+     * 获取
+     * @return connectionPool_maxPoolSize
+     */
+    public int getConnectionPool_maxPoolSize() {
+        return connectionPool_maxPoolSize;
+    }
+
+    /**
+     * 设置
+     * @param connectionPool_maxPoolSize
+     */
+    public void setConnectionPool_maxPoolSize(int connectionPool_maxPoolSize) {
+        this.connectionPool_maxPoolSize = connectionPool_maxPoolSize;
+    }
+
+    /**
+     * 获取
+     * @return connectionPool_minIdle
+     */
+    public int getConnectionPool_minIdle() {
+        return connectionPool_minIdle;
+    }
+
+    /**
+     * 设置
+     * @param connectionPool_minIdle
+     */
+    public void setConnectionPool_minIdle(int connectionPool_minIdle) {
+        this.connectionPool_minIdle = connectionPool_minIdle;
+    }
+
+    /**
+     * 获取
+     * @return connectionPool_connectionTimeout
+     */
+    public int getConnectionPool_connectionTimeout() {
+        return connectionPool_connectionTimeout;
+    }
+
+    /**
+     * 设置
+     * @param connectionPool_connectionTimeout
+     */
+    public void setConnectionPool_connectionTimeout(int connectionPool_connectionTimeout) {
+        this.connectionPool_connectionTimeout = connectionPool_connectionTimeout;
+    }
+
+    /**
+     * 获取
+     * @return connectionPool_idleTimeout
+     */
+    public int getConnectionPool_idleTimeout() {
+        return connectionPool_idleTimeout;
+    }
+
+    /**
+     * 设置
+     * @param connectionPool_idleTimeout
+     */
+    public void setConnectionPool_idleTimeout(int connectionPool_idleTimeout) {
+        this.connectionPool_idleTimeout = connectionPool_idleTimeout;
+    }
+
+    /**
+     * 获取
+     * @return connectionPool_maxLifetime
+     */
+    public int getConnectionPool_maxLifetime() {
+        return connectionPool_maxLifetime;
+    }
+
+    /**
+     * 设置
+     * @param connectionPool_maxLifetime
+     */
+    public void setConnectionPool_maxLifetime(int connectionPool_maxLifetime) {
+        this.connectionPool_maxLifetime = connectionPool_maxLifetime;
+    }
+
+    /**
+     * 获取
+     * @return cache_enabled
+     */
+    public boolean isCache_enabled() {
+        return cache_enabled;
+    }
+
+    /**
+     * 设置
+     * @param cache_enabled
+     */
+    public void setCache_enabled(boolean cache_enabled) {
+        this.cache_enabled = cache_enabled;
+    }
+
+    /**
+     * 获取
+     * @return cache_ttl
+     */
+    public int getCache_ttl() {
+        return cache_ttl;
+    }
+
+    /**
+     * 设置
+     * @param cache_ttl
+     */
+    public void setCache_ttl(int cache_ttl) {
+        this.cache_ttl = cache_ttl;
+    }
+
+    public String toString() {
+        return "StorgeConfiguration{storageMethod_playerData = " + storageMethod_playerData + ", storageMethod_questData = " + storageMethod_questData + ", sqlite_file = " + sqlite_file + ", mysql_host = " + mysql_host + ", mysql_port = " + mysql_port + ", mysql_database = " + mysql_database + ", mysql_username = " + mysql_username + ", mysql_password = " + mysql_password + ", mysql_useSsl = " + mysql_useSsl + ", mysql_poolSize = " + mysql_poolSize + ", connectionPool_maxPoolSize = " + connectionPool_maxPoolSize + ", connectionPool_minIdle = " + connectionPool_minIdle + ", connectionPool_connectionTimeout = " + connectionPool_connectionTimeout + ", connectionPool_idleTimeout = " + connectionPool_idleTimeout + ", connectionPool_maxLifetime = " + connectionPool_maxLifetime + ", cache_enabled = " + cache_enabled + ", cache_ttl = " + cache_ttl + "}";
+    }
 }
