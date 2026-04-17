2360633a1679c5f5ad0187ce8377015a95298186
yvmou_ <yvmou@outlook.com>
2026-04-17 22:11:21 +0800

    refactor:重构 TaskProgress 和 TaskStatus - 添加 objectiveProgress Map

--- api/src/main/java/com/playerPlugin/playerTaskX/api/Enum/PTXTaskStatus.java
+++ api/src/main/java/com/playerPlugin/playerTaskX/api/Enum/PTXTaskStatus.java
@@ -1,9 +1,8 @@
 package com.playerPlugin.playerTaskX.api.Enum;
 
 public enum PTXTaskStatus {
-    NOT_STARTED, // 任务未开始
-    IN_PROGRESS, // 任务进行中
-    COMPLETED, // 任务完成
-    FAILED, // 任务失败
-    EXPIRED // 任务过期
+    IN_PROGRESS,
+    COMPLETED,
+    CLAIMED,
+    ABANDONED
 }
