14d88f7c0e1532ce9be0e51fde1a8bf31bc216e1
yvmou_ <yvmou@outlook.com>
2026-04-19 00:53:02 +0800

    Update GraphEventListener.java

--- core/src/main/java/com/playerPlugin/playerTaskX/listener/GraphEventListener.java
+++ core/src/main/java/com/playerPlugin/playerTaskX/listener/GraphEventListener.java
@@ -6,9 +6,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 
 public class GraphEventListener implements Listener {
     private final QuestEngine questEngine;
