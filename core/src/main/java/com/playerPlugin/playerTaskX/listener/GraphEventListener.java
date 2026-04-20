0ace43d1fd6fb4cd71399b976a92caf2b215f7ac
yvmou_ <yvmou@outlook.com>
2026-04-20 21:30:27 +0800

    修复调度器错误

--- core/src/main/java/com/playerPlugin/playerTaskX/listener/GraphEventListener.java
+++ core/src/main/java/com/playerPlugin/playerTaskX/listener/GraphEventListener.java
@@ -1,5 +1,6 @@
 package com.playerPlugin.playerTaskX.listener;
 
+import cn.yvmou.ylib.YLib;
 import com.playerPlugin.playerTaskX.engine.QuestEngine;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -21,7 +22,7 @@ public class GraphEventListener implements Listener {
     public void onEntityDeath(EntityDeathEvent event) {
         Player killer = event.getEntity().getKiller();
         if (killer != null) {
-            Bukkit.getScheduler().runTaskLater(null, () -> {
+            YLib.getyLib().getScheduler().runLater(() -> {
                 questEngine.handleEvent(killer, event);
             }, 1L);
         }
@@ -30,7 +31,7 @@ public class GraphEventListener implements Listener {
     @EventHandler(priority = EventPriority.MONITOR)
     public void onBlockBreak(BlockBreakEvent event) {
         Player player = event.getPlayer();
-        Bukkit.getScheduler().runTaskLater(null, () -> {
+        YLib.getyLib().getScheduler().runLater(() -> {
             questEngine.handleEvent(player, event);
         }, 1L);
     }
@@ -38,7 +39,7 @@ public class GraphEventListener implements Listener {
     @EventHandler(priority = EventPriority.MONITOR)
     public void onBlockPlace(BlockPlaceEvent event) {
         Player player = event.getPlayer();
-        Bukkit.getScheduler().runTaskLater(null, () -> {
+        YLib.getyLib().getScheduler().runLater(() -> {
             questEngine.handleEvent(player, event);
         }, 1L);
     }
